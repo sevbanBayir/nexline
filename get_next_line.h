7eda2b95632d26b82f428a6670038c7703b4c6e8
Sevban Bayir <sevbanbuyer@gmail.com>
2023-01-20 10:59:49 +0300

    intiial

---
+++ get_next_line.h
@@ -0,0 +1,9 @@
+#ifndef GET_NEXT_LINE.H
+# define GET_NEXT_LINE.H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+char *get_next_line(int fd);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,20 @@
+//is new line ?
+int ft_is_newline()
+
+
+
+//concatenate strings if there are more than one
+
+
+
+
+//strlen
+
+
+
+
+//get line
+
+
+
+//update string
